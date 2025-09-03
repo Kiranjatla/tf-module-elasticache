f121b18cf98cdfaabba680fa829eebc5939562d8
KIRAN <ks@gmail.com>
2025-09-03 07:06:05 +0530

    gateways

--- main.tf
+++ main.tf
@@ -1,8 +1,36 @@
-resource "aws_elasticache_cluster" "main" {
-  cluster_id           = "${var.env}-${var.name}-elasticache"
-  engine               = var.engine
-  engine_version       = var.engine_version
-  node_type            = var.node_type
-  num_cache_nodes      = var.num_cache_nodes
-  parameter_group_name = aws_elasticache_parameter_group.default.name
-}
+#resource "aws_elasticache_cluster" "main" {
+#  cluster_id           = "${var.env}-${var.name}-elasticache"
+#  engine               = var.engine
+#  engine_version       = var.engine_version
+#  node_type            = var.node_type
+#  num_cache_nodes      = var.num_cache_nodes
+#  parameter_group_name = aws_elasticache_parameter_group.default.name
+#}
+#
+#resource "aws_elasticache_parameter_group" "default" {
+#  name   = "${var.env}-${var.name}-elasticache"
+#  family = "redis6.x"
+#}
+#
+#resource "aws_elasticache_subnet_group" "default" {
+#  name       = "${var.env}-${var.name}-elasticache"
+#  subnet_ids = var.subnets
+#}
+
+
+
+
+
+
+
+
+
+
+#resource "aws_elasticache_cluster" "main" {
+#  cluster_id           = "${var.env}-${var.name}-elasticache"
+#  engine               = var.engine
+#  engine_version       = var.engine_version
+#  node_type            = var.node_type
+#  num_cache_nodes      = var.num_cache_nodes
+#  parameter_group_name = aws_elasticache_parameter_group.default.name
+#}
--- vars.tf
+++ vars.tf
@@ -1,2 +1,16 @@
-variable "elasticache" {}
-variable "env" {}
+#variable "env" {}
+#variable "name" {}
+#variable "engine" {}
+#variable "engine_version" {}
+#variable "node_type" {}
+#variable "num_cache_nodes" {}
+#variable "subnets" {}
+
+
+
+
+
+
+
+#variable "elasticache" {}
+#variable "env" {}
