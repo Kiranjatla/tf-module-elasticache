2c815fcd0983e0a499238cea1183d866fe1ff03e
KIRAN <ks@gmail.com>
2025-09-03 07:24:55 +0530

    elasticache

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
-#variable "env" {}
-#variable "name" {}
-#variable "engine" {}
-#variable "engine_version" {}
-#variable "node_type" {}
-#variable "num_cache_nodes" {}
-#variable "subnets" {}
+variable "env" {}
+variable "name" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "node_type" {}
+variable "num_cache_nodes" {}
+variable "subnets" {}
 
 
 
