6dcdad49eea7b02f4f233ad002e25944ce022971
KIRAN <ks@gmail.com>
2025-09-03 07:58:20 +0530

    elasticache

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_elasticache_cluster" "main" {
   node_type            = var.node_type
   num_cache_nodes      = var.num_cache_nodes
   parameter_group_name = aws_elasticache_parameter_group.default.name
+  port                 = 6379
 }
 
 resource "aws_elasticache_parameter_group" "default" {
